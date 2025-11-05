0d35a675c63670171f2d489a9477ef21695d2181
kumari203 <125052101+kumari203@users.noreply.github.com>
2025-11-05 17:16:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -42,6 +43,7 @@ def add_user():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
